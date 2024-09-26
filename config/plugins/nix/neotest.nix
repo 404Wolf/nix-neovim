7d4fc92fda971915d9657cf61fc21d4b9b537ba6
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-09-26 01:11:08 -0400

    Add dap ui bind

--- config/plugins/nix/neotest.nix
+++ config/plugins/nix/neotest.nix
@@ -24,10 +24,25 @@
         noremap = true;
       };
     }
-
     {
       key = "<Leader>tnd";
       action = "require'neotest'.run.run({strategy = 'dap'})";
+      mode = "n";
+      options = {
+        noremap = true;
+      };
+    }
+    {
+      key = "<Leader>tnd";
+      action = "require'neotest'.run.run({strategy = 'dap'})";
+      mode = "n";
+      options = {
+        noremap = true;
+      };
+    }
+    {
+      key = "<C-p>";
+      action = "require'dapui'.toggle()";
       mode = "n";
       options = {
         noremap = true;
