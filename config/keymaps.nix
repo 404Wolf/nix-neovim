03c2c6cea2abc5c9aca756d5471c90a7e5357885
Wolf Mermelstein <wolf@404wolf.com>
2025-09-01 23:02:30 -0400

    Format code

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,32 +1,57 @@
 {
-  keymaps =
-    (builtins.genList (
-      n:
-      let
-        tabNumber = toString (n + 1);
-      in
-      {
-        key = "<A-${tabNumber}>";
-        mode = [
-          "n"
-          "v"
-        ];
-        action = "${tabNumber}gt";
-        options.desc = "Go to tab ${tabNumber}";
-      }
-    ) 9)
-    ++ [
-      {
-        key = "<A-t>";
-        action = "<cmd>tabnew<cr>";
-      }
-      {
-        key = "<A-c>";
-        action = "<cmd>tabclose<cr>";
-      }
-      {
-        key = "<C-q>";
-        action = "<cmd>q<cr>";
-      }
-    ];
+  keymaps = [
+    # Toggle nvim-tree with Ctrl+n
+    {
+      key = "<C-n>";
+      action = "<cmd>NvimTreeToggle<CR>";
+      mode = "n";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Toggle NvimTree";
+      };
+    }
+
+    # Window navigation with Ctrl+h/j/k/l
+    {
+      key = "<C-h>";
+      action = "<C-w>h";
+      mode = "n";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Move to left window";
+      };
+    }
+    {
+      key = "<C-j>";
+      action = "<C-w>j";
+      mode = "n";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Move to bottom window";
+      };
+    }
+    {
+      key = "<C-k>";
+      action = "<C-w>k";
+      mode = "n";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Move to top window";
+      };
+    }
+    {
+      key = "<C-l>";
+      action = "<C-w>l";
+      mode = "n";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Move to right window";
+      };
+    }
+  ];
 }
