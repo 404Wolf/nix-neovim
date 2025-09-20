979f3b2d3498f884cd88064dbeda1152af706a51
Wolf Mermelstein <wolf@404wolf.com>
2025-09-20 09:24:42 -0400

    Update cmp bind

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -4,14 +4,12 @@
       enable = true;
       settings = {
         snippet.expand = "function(args) return end";
-
         mapping = {
           "<C-Space>" = "cmp.mapping.complete()";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<CR>" = "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })";
           "<Down>" = "cmp.mapping.select_next_item()";
           "<Up>" = "cmp.mapping.select_prev_item()";
         };
-
         sources = [
           { name = "nvim_lsp"; }
           { name = "buffer"; }
